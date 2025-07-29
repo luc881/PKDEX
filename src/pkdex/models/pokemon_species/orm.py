d838b4bfb238299a5c1e575433bbe219e1997ebd
Luciano <ljuarezs2000@alumno.ipn.mx>
2025-07-29 02:15:00 -0600

    Create species SQLAlchemy model

---
+++ src/pkdex/models/pokemon_species/orm.py
@@ -0,0 +1,15 @@
+from sqlalchemy import ForeignKey
+from ...db.session import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+class PKMSpecies(Base):
+    __tablename__ = "pkmspecies"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    national_dex_number: Mapped[int] = mapped_column(unique=True, index=True)
+    evolution_line_id: Mapped[int] = mapped_column(unique=True, index=True)
+    name_es: Mapped[str]
+    name_jp: Mapped[str]
+    generation: Mapped[int]
+    has_gender_differences: Mapped[bool]
+    description: Mapped[str]
